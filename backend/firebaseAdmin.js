f4ee355c97c3610b9469cfbf7812200839acd6e4
gabmacedo <gabmacedo33gmail.com>
2025-10-30 00:06:40 -0300

    feat: add firebase auth, style on auth pages

--- backend/firebaseAdmin.js
+++ backend/firebaseAdmin.js
@@ -1,9 +1,10 @@
 import admin from "firebase-admin";
 import { readFileSync } from "fs";
 
-const serviceAccount = JSON.parse(
-  readFileSync("./serviceAccountKey.json", "utf-8")
-);
+const serviceAccount = JSON
+  .parse
+  // readFileSync("./serviceAccountKey.json", "utf-8")
+  ();
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
