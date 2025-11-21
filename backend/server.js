f650ec1af98ec69c9048469c8f2faf7d5210f715
Gabriel Macedo <gabmacedo33@gmail.com>
2025-11-21 12:29:32 -0300

    delete backend side

--- backend/server.js
+++
@@ -1,9 +0,0 @@
-import Fastify from "fastify";
-
-const app = Fastify({ logger: true });
-
-app.get("/", async (request, reply) => {
-  return { hello: "world" };
-});
-
-app.listen({ port: 3000 });
